f38d2cdcc48f3a09f2fb915bc05fa5cbf16c8df9
no12125nades-oss <no12125nades@gmail.com>
2026-06-13 07:04:20 +0300

    Update connection.ts

--- api/queries/connection.ts
+++ api/queries/connection.ts
@@ -1,17 +1,24 @@
 import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 import { env } from "../lib/env";
 import * as schema from "@db/schema";
 import * as relations from "@db/relations";
 
 const fullSchema = { ...schema, ...relations };
 
-let instance: ReturnType<typeof drizzle<typeof fullSchema>>;
+let instance: ReturnType<typeof drizzle<typeof fullSchema>> | null = null;
 
 export function getDb() {
   if (!instance) {
-    instance = drizzle(env.databaseUrl, {
-      mode: "planetscale",
+    const poolConnection = mysql.createPool({
+      uri: env.databaseUrl,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+    instance = drizzle(poolConnection, {
       schema: fullSchema,
+      mode: "default",
     });
   }
   return instance;
